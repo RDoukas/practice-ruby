8487c42aa0becfe24433af0a278a131bda0e627a
Becca <rrdoukas@gmail.com>
2020-05-20 12:31:35 -0500

    Creates store hash

---
+++ store-item.rb
@@ -0,0 +1,23 @@
+blush = {
+  color: "Pink Peach Pop",
+  price: 30,
+  brand: "Too Faced",
+  store_section: "Makeup",
+  body_part: "Cheeks"
+}
+
+eye_shadow = {
+  color: "Desert Dreams",
+  price: 65,
+  brand: "Hude",
+  store_section: "Makeup",
+  body_part: "eyes"
+}
+
+moisturizer = {
+  color: "none",
+  price: 15,
+  brand: "The Ordinary",
+  store_section: "Skincare",
+  body_part: "Face"
+}
