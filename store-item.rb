1aea109d4bae8bcd79a60ee2ccc8ad5b8a5ce868
Becca <rrdoukas@gmail.com>
2020-05-20 13:59:17 -0500

    adds methods to Store class

--- store-item.rb
+++ store-item.rb
@@ -1,23 +1,44 @@
-blush = {
-  color: "Pink Peach Pop",
-  price: 30,
-  brand: "Too Faced",
-  store_section: "Makeup",
-  body_part: "Cheeks"
-}
+class Store 
 
-eye_shadow = {
-  color: "Desert Dreams",
-  price: 65,
-  brand: "Hude",
-  store_section: "Makeup",
-  body_part: "eyes"
-}
+  def initialize(color, price, brand, store_section, body_part)
+    @color = color,
+    @price = price,
+    @brand = brand, 
+    @store_section = store_section, 
+    @body_part = body_part
+  end 
 
-moisturizer = {
-  color: "none",
-  price: 15,
-  brand: "The Ordinary",
-  store_section: "Skincare",
-  body_part: "Face"
-}
+
+
+  def color
+    @color
+  end 
+
+  def price
+    @price 
+  end 
+
+  def brand
+    @brand
+  end 
+
+  def store_section
+    @store_section
+  end 
+
+  def body_part 
+    @body_part
+  end 
+
+  def season_sale
+    @price = price - (price * 0.15)
+  end 
+
+end 
+  
+
+eye_shadow = Store.new("Dessert Dreams", 65, "Huda", "Makeup", "Eyes")
+blush = Store.new("Pink Peach Pop", 30, "Too Faced", "Makeup", "Cheeks")
+moisturizer = Store.new("none", 15, "The Ordinary", "Skincare", "Face")
+
+p eye_shadow.season_sale
