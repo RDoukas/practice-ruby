37735445996188c36b98d15660e140f5febf7b9c
Becca <rrdoukas@gmail.com>
2020-05-20 14:01:46 -0500

    updates instance prints

--- store-item.rb
+++ store-item.rb
@@ -41,4 +41,7 @@ eye_shadow = Store.new("Dessert Dreams", 65, "Huda", "Makeup", "Eyes")
 blush = Store.new("Pink Peach Pop", 30, "Too Faced", "Makeup", "Cheeks")
 moisturizer = Store.new("none", 15, "The Ordinary", "Skincare", "Face")
 
+p eye_shadow.price
 p eye_shadow.season_sale
+p blush.body_part
+
